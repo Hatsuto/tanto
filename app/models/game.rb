9fe51bbf71b66fd2477ffdf3c787519acbb77638
David Chau <davidchauprogrammer@gmail.com>
2016-06-27 10:24:04 -0700

    Moved gameplay module from modules folder to models folder. Created functions parse_instruction and create_instruction_hash to parse instruction and create the hash of instructions after the parsing.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,18 @@
 class Game < ActiveRecord::Base
-  def parse_instruction(instruction_string)
+include Gameplay
+  def self.parse_instruction(instruction_string)
     instruction_string.split(";").map do |instruction|
       instruction.split(":")
     end
+  end
+
+  def create_instruction_hash
+    @instruction = {
+      "draw" => draw,
+      "shuffle" => shuffle,
+      "discard" => discard,
+      "gain_card" => gain_card,
+      "return_card" => return_card
+    }
   end
 end
