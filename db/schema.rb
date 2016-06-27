be8f283deb552c15e29f48002ed604234b7ead31
David Chau <davidchauprogrammer@gmail.com>
2016-06-27 09:40:58 -0700

    migrated the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613201233) do
+ActiveRecord::Schema.define(version: 20160627163550) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "name"
+    t.text     "card_text"
+    t.integer  "victory_points"
+    t.integer  "employ_cost"
+    t.string   "job_title"
+    t.text     "ability"
+    t.text     "special_effect"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
